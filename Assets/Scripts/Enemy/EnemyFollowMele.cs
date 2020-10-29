bfb7ce4e3c262732f0470d7cf88be8a792a6d8b1
GreyZar37 <52458016+GreyZar37@users.noreply.github.com>
2020-10-29 15:38:05 +0100

    h

---
+++ Assets/Scripts/Enemy/EnemyFollowMele.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollowMele: MonoBehaviour
+{
+
+    public float speed;
+    public Transform target;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    public float coolDown;
+    public float coolDownTimer;
+
+    public int distence;
+
+   
+
+
+    void Start()
+    {
+         rb = this.GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+   
+    void Update()
+    {
+
+
+        if (coolDownTimer > 0)
+        {
+            coolDownTimer -= Time.deltaTime;
+        }
+        if (coolDownTimer <= 0)
+        {
+            coolDownTimer = 0;
+        }
+
+       
+        Vector2 direction = new Vector2(target.position.x - transform.position.x, target.position.y - transform.position.y);
+        transform.up = direction;
+
+        if (Vector2.Distance(transform.position, target.position) > distence)
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        if(Vector2.Distance(transform.position, target.position) < distence && coolDownTimer == 0)
+        {
+            gameObject.GetComponent<EnemyShooting>().shoot();
+            coolDownTimer = coolDown;
+          
+        }
+
+       
+
+
+    }
+
+ 
+
+}
